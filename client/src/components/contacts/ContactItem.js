9e5aebb69099da0bc3f4249b1ff032271d0da331
inuzen <condddr@gmail.com>
2019-10-05 21:24:47 +0300

    finished crud

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -5,11 +5,12 @@ import ContactContext from '../../context/contact/contactContext'
 const ContactItem = ({contact}) => {
   const contactContext = useContext(ContactContext);
   const {deleteContact, setCurrent, clearCurrent} = contactContext;
-  const {id, name, email, phone, type} = contact;
+  const {_id, name, email, phone, type} = contact;// _id cause of mongo
 
   const onDelete = e => {
     e.preventDefault();
-    deleteContact(id);
+
+    deleteContact(_id);
     clearCurrent();
   }
 
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,30 +1,34 @@
-import React, {Fragment, useContext} from 'react';
-import PropTypes from 'prop-types';
+import React, {Fragment, useContext, useEffect} from 'react';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
-
+import Spinner from '../layout/Spinner';
 const Contacts = (props) => {
   const contactContext = useContext(ContactContext);
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-  if (contacts.length===0) {
+  useEffect(() => {
+    getContacts();
+    //eslint-disable-next-line
+  },[]);//empty brackets = at the beginning
+  if (contacts!==null && contacts.length===0 && !loading) {
     return <h4>Please add a contact</h4>
   }
   return (
     <Fragment>
-      <TransitionGroup>
-      {filtered!==null
-        ? filtered.map((contact) => (<CSSTransition key={contact.id} timeout={500} classNames='item'><ContactItem contact={contact}/></CSSTransition>)) //if dont want animation for filter then just remove it from this line
-        : contacts.map(contact => (<CSSTransition key={contact.id} timeout={500} classNames='item'><ContactItem contact={contact}/></CSSTransition>))
-      }
-    </TransitionGroup>
+      {contacts !== null && !loading
+        ? (
+        <TransitionGroup>
+        {filtered!==null
+          ? filtered.map((contact) => (<CSSTransition key={contact._id} timeout={500} classNames='item'><ContactItem contact={contact}/></CSSTransition>)) //if dont want animation for filter then just remove it from this line
+          : contacts.map(contact => (<CSSTransition key={contact._id} timeout={500} classNames='item'><ContactItem contact={contact}/></CSSTransition>))
+        }
+      </TransitionGroup>
+    )
+      : (<Spinner/>)}
+
     </Fragment>
   )
 }
-
-Contacts.propTypes = {
-
-};
 
 export default Contacts;
